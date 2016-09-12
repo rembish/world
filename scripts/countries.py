da850ff459d3fd96238b197d9939afff7f2b8cf5
Aleksey Rembish <alex@rembish.org>
2016-09-12 13:28:12 +0200

    GeonameIds for countries

--- scripts/countries.py
+++ scripts/countries.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 from __future__ import print_function
-from csv import reader
+from csv import reader, DictWriter
 from requests import get
 from sys import argv
 
@@ -16,8 +16,16 @@ class Country(object):
 
         self.centroid = data["lat"], data["lng"]
         self.bounding_box = (
-            data["bbox"]["west"], data["bbox"]["north"],
-            data["bbox"]["east"], data["bbox"]["south"]
+            data["bbox"]["north"], data["bbox"]["east"],
+            data["bbox"]["south"], data["bbox"]["west"],
+        )
+
+    def get_row(self):
+        return (
+            ",".join(self.continents), self.iso2, self.geoname_id,
+            self.centroid[0], self.centroid[1],
+            self.bounding_box[0], self.bounding_box[1], self.bounding_box[2], self.bounding_box[3],
+            self.names["en"], self.names["cs"], self.names["ru"],
         )
 
 countries = []
@@ -25,12 +33,15 @@ with open("../countries.csv") as fd:
     for row in reader(fd, delimiter=";"):
         if not row:
             continue
+        if len(row[1]) != 2:
+            continue  # header
 
         geoname_id = row[2]
         if geoname_id == "-":
             geoname_id = None
         geoname_id = geoname_id or None
 
+        print("Getting data for {}".format(row[-3]))
         if not geoname_id:
             result = get("http://api.geonames.org/countryInfoJSON", params={
                 "username": GEONAMES_USER,
@@ -42,14 +53,26 @@ with open("../countries.csv") as fd:
             "username": GEONAMES_USER,
             "geonameId": geoname_id,
             "style": "full",
-        }).json()["geoname"]
+        }).json()
 
         country = Country(result)
         country.continents = row[0].split(",")
+        country.iso2 = row[1].upper()
         country.names = {
-            "en": row[3],
-            "cs": row[4],
-            "ru": row[5],
+            "en": row[-3],
+            "cs": row[-2],
+            "ru": row[-1],
         }
         countries.append(country)
 
+with open('../countries.csv', "wt") as fd:
+    writer = DictWriter(fd, [
+        "continents", "iso2", "geoname_id",
+        "latitude", "longitude",
+        "north", "east", "south", "west",
+        "en", "cs", "ru"
+    ], delimiter=";")
+    writer.writeheader()
+
+    for country in countries:
+        writer.writer.writerow(country.get_row())
